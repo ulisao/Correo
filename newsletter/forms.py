eff8917e9cdafa7f270c21dde992825c4f1d69f0
ulisao <ulisessbaretta@gmail.com>
2022-09-15 22:33:26 -0300

    Creacion newsletter, modelo, funcion envio de mail

---
+++ newsletter/forms.py
@@ -0,0 +1,13 @@
+import email
+from django import forms
+from .models import Newsletter, NewsletterUser
+
+class NewsletterUserSignUpForm(forms.ModelForm):
+    class Meta:
+        model = NewsletterUser
+        fields = ['email']
+
+class NewsletterCreationForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = ['name', 'subject', 'body', 'email']
---
+++ newsletter/views.py
@@ -0,0 +1,36 @@
+from django.core.mail import EmailMessage, send_mail
+from django.core.checks import messages
+from django.shortcuts import render
+from .forms import NewsletterUserSignUpForm
+from .models import NewsletterUser
+from django.conf import settings
+from django.template.loader import render_to_string
+
+# Create your views here.
+def Newsletter_SignUp(request): #Enviamos correo electronico
+    form = NewsletterUserSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewsletterUser.objects.filter(email=instance.email).exists(): #En caso de que el mail ya este registrado
+            messages.warning(request, 'Este email ya se encuentra registrado')
+
+        else: #En caso contrario guardamos el mail y enviamos correo
+            instance.save()
+            messages.success(request, 'Hemos enviado un mail a tu correo, revisa la casilla de spam')
+            subject = "Bienvenido a rutasdev"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = [instance.email]
+            html_template = 'newsletters/email/welcome.html'
+            html_message = render_to_string(html_template)
+            message = EmailMessage(subject,html_message,from_email,to_email)
+            message.content_subtype = 'html'
+            message.send()
+
+    context = {
+        'form':form,
+    }
+    
+    return render(request, 'start.html', context)
+
+
